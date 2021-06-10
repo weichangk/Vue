16ebaa744c5912dc3b85e9d750159b7e74ad2110
weichangk <weichangk@qq.com>
2021-06-10 23:51:53 +0800

    webpack

--- 02 webpack/02 webpack配置/src/main.js
+++ 02 webpack/02 webpack配置/src/main.js
@@ -1,9 +1,11 @@
 console.log("aaa");
 console.log("bbb");
 
-import {add, mul} from './aaa.js'
+import {add, mul} from './js/aaa.js'
 
 console.log(add(1, 2));
 console.log(mul(3, 3));
 
-
+//依赖css模块
+//require('./css/normal.css')
+import css from "./css/normal.css";
